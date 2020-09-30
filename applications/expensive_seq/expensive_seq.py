68b45577513c10ad45e6b5ae6b53265e20d7d5d7
wildcard329 <enkiduwildman@gmail.com>
2020-09-30 12:37:04 -0700

    expensive_seq passing

--- applications/expensive_seq/expensive_seq.py
+++ applications/expensive_seq/expensive_seq.py
@@ -1,10 +1,20 @@
 # Your code here
-
+table = {}
+"""
+exps(x, y, z) =
+     if x <= 0: y + z
+     if x >  0: exps(x-1,y+1,z) + exps(x-2,y+2,z*2) + exps(x-3,y+3,z*3)
+"""
 
 def expensive_seq(x, y, z):
     # Your code here
+    if x <= 0:
+        return y + z
+    else:
+        if (x, y, z) not in table:
+            table[(x, y, z)] = expensive_seq(x-1, y+1, z) + expensive_seq(x-2, y+2, z*2) + expensive_seq(x-3, y+3, z*3)
 
-
+    return table[(x, y, z)]
 
 if __name__ == "__main__":
     for i in range(10):
